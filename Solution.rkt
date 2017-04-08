c1461f3508fc2443bdd4fb963e627524d63807bb
JasonThorne <Jason@JAY-PC>
2017-04-08 22:15:48 +0100

    testing with 2 numbers

--- Solution.rkt
+++ Solution.rkt
@@ -1,3 +1,24 @@
 #lang racket
 
-; initial set up
+;avaliable opperators:
+(define ops '(+-*/))
+
+;temp values for testing:
+(define a 5)
+(define b 25)
+
+;target number:
+(define t 125)
+
+;all possibilities for 2 numbers:
+(+ a b)
+(+ b a)
+
+(- a b)
+(- b a)
+
+(* a b)
+(* b a)
+
+(/ a b)
+(/ b a)
